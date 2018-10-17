9f4cb603c3ffdc86bd7d3b3bc7c3cbc115d5f850
unknown <heshamelsherif97@gmail.com>
2018-10-17 10:59:12 +0200

    Added comments

--- src/Q1.java
+++ src/Q1.java
@@ -7,7 +7,7 @@ import org.opencv.imgproc.Imgproc;
 
 public class Q1 extends CvAssignment1{
 	
-	
+	//Apply Contrast effect on an image by loopingon the pixels and multiplying the pixel value by a constant
 	public Mat applyBrightness1(Mat img, double contrast, double brightness) {
 		Mat result = new Mat(img.rows(), img.cols(), img.type());
 		for (int i = 0; i < result.rows(); i++) {
@@ -24,17 +24,22 @@ public class Q1 extends CvAssignment1{
 		return result;
 	}
 	
+	//Reflect an image on y-axis
 	public Mat reflectImg(Mat img) {
 		Mat result = new Mat();
 		Core.flip(img, result, 1);
 		return result;
 	}
+	
+	//Blend 2 images 
 	public static Mat add2Images(Mat img1, Mat img2) {
 		Mat result = new Mat();
 		double alpha = 0.8;
 		Core.addWeighted(img1, alpha, img2, 1.0 - alpha, 0, result);
 		return result;
 	}
+	
+	//Translating an image using affine transformation
 	public Mat translate(Mat img, int tx, int ty) {
 		Mat out = new Mat();
 		Mat trans = new Mat(2, 3, CvType.CV_32F);
@@ -45,6 +50,8 @@ public class Q1 extends CvAssignment1{
 		return out;
 	}
 	
+	
+	//Scaling an Image
 	public static Mat scaleImg(Mat img, Mat src) {
 		  Mat result = new Mat();
 	      Size size = new Size(src.width(), src.height());
@@ -55,11 +62,23 @@ public class Q1 extends CvAssignment1{
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		Q1 cv1 = new Q1();
+		
+		//Load Image1
 		Mat img1 = cv1.loadImage("A1I/Q1I1.png", Imgcodecs.CV_LOAD_IMAGE_COLOR);
+		
+		//Load Image2
 		Mat img2 = cv1.loadImage("A1I/Q1I2.jpg", Imgcodecs.CV_LOAD_IMAGE_COLOR);
+		
+		//Resize Image 2 to the size of image 1 
 		Imgproc.resize(img2, img2, new Size(img1.width(), img1.height()), 0, 0, Imgproc.INTER_AREA);
+		
+		//Apply a fade out contrast effect on image 1
 		img1 = cv1.applyBrightness1(img1, 5, 0);
+		
+		//Translate and reflect image 2
 		img2 = cv1.translate(scaleImg(cv1.reflectImg(img2), img1), 150, 0);
+		
+		//Save the result of the blending of the two images after processing
 		cv1.saveImage("output/outQ1-O1.jpg", add2Images(img1, img2));
 	}
 }
--- src/Q3.java
+++ src/Q3.java
@@ -10,12 +10,13 @@ import org.opencv.imgproc.Imgproc;
 public class Q3 extends CvAssignment1{
 	
 	public static Mat add2Images(Mat img1, Mat img2) {
+		  //Source points
 	      Point p1 = new Point( 0,0 );
 	      Point p2 = new Point( img1.cols() - 1, 0 );
 	      Point p3 = new Point( 0, img1.rows() - 1 );
 	      Point p4 = new Point( img1.cols() - 1, img1.rows() - 1 );
 	      
-	      
+	      //Destination Points
 	      Point p5 = new Point( 163, 35 );
 	      Point p6 = new Point( 470, 70 );
 	      Point p7 = new Point( 158, 390 );
@@ -23,12 +24,16 @@ public class Q3 extends CvAssignment1{
 	      
 	      MatOfPoint2f ma1 = new MatOfPoint2f(p1,p2,p3,p4);
 	      MatOfPoint2f ma2 = new MatOfPoint2f(p5,p6,p7,p8);
+	      
+	      //Finding the homography that turns source to destination
 	      Mat homography = Calib3d.findHomography(ma1, ma2);
 	      
 	      Mat result = new Mat();
+	      
 	      // Warp source image to destination based on homography
 	      Imgproc.warpPerspective(img1, result, homography, img2.size());
 	      
+	      //Adding the 2 images together
 	      for (int i = 0; i < result.rows(); i++) {
 			for (int j = 0; j < result.cols(); j++) {
 				double[] rgb = result.get(i, j);
@@ -46,6 +51,7 @@ public class Q3 extends CvAssignment1{
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		Q3 cv1 = new Q3();
+		//Load Input Images
 		Mat img1 = cv1.loadImage("A1I/Q2I1.jpg", Imgcodecs.CV_LOAD_IMAGE_COLOR);
 		Mat img2 = cv1.loadImage("A1I/Q3I1.jpg", Imgcodecs.CV_LOAD_IMAGE_COLOR);
 		Mat out = add2Images(img1, img2);
